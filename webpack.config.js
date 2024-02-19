5a6e59a398df4438e8d81aa02194e6fa68601c3a
gvibe3 <110994525+Gvibe3@users.noreply.github.com>
2024-02-19 15:58:15 +0300

    htmlplugin added

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,25 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: {
+    index: "./src/index.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "multi-step-form",
+      favicon: "./src/assets/images/favicon-32x32.png",
+      filename: "./dist/index.html",
+      template: "./src/template.html",
+    }),
+  ],
   devtool: "inline-source-map",
   mode: "development",
 
   output: {
-    filename: "main.js",
+    filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
+    clean: true,
   },
   module: {
     rules: [
