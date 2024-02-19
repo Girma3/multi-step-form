350c2f5a15e5d5b2cb23f6a571189e0a1e194701
gvibe3 <110994525+Gvibe3@users.noreply.github.com>
2024-02-19 07:29:06 +0300

    image added

--- src/tabs.js
+++ src/tabs.js
@@ -92,6 +92,7 @@ const tabOne = function (obj) {
   const personalInfo = ` 
     <div class="step-one">
         <div class="form-header">
+        <div class="icon-holder"><img src="../src/assets/images/icon-advanced.svg" alt="icon for advanced plan" ></div>
             <h1>Personal Info</h1>
             <p class="instruction">please provide your name,email adress and phone number</p>
         </div>
